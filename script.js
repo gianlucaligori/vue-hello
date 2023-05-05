99b1181234c2297e65e8e92699c4fc370ff86a25
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-05-05 16:43:44 +0200

    Frank

---
+++ script.js
@@ -0,0 +1,18 @@
+// Esercizio di oggi: Vue Hello
+// nome repo: vue-hello
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+const app = Vue.createApp({
+    data() {
+      return {
+        message: 'Frank Underwood',
+        image: "img/what.jpg"
+      }
+    }
+  })
+  
+  app.mount('#app')
